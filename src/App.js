89abce3edf0ffe6ce11f25da0aec25538c92809d
Abdul Rehman <102576102+BetterLogicTeam@users.noreply.github.com>
2022-11-30 23:17:04 +0500

    web3 import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
+
 import './App.css';
 import Landing from './Components/Landing/Landing';
 
 function App() {
   return (
     <div className="content">
+ 
     <Landing />
     </div>
   );
